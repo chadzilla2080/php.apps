f97b4382d3073ab59fc1e7ad4b7429bcbb7c0b70
Chad Buie <chadbuie@gmail.com>
2016-02-27 11:18:52 -0500

    Setting in my first apps!

---
+++ first.app.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>First PHP APP | Hello World | First PHP APP</title>
+    <body>
+         <h1>First PHP Application</h1>
+         <p>
+             <?php echo "Hello World!" ; ?>
+         </p>
+    </body>
+</head>
+</html>
